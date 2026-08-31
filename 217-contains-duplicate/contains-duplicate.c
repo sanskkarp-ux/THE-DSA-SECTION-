fec9f69bfad752d06d29b385d7a5349db2844666
Sanxkar <sanskkarp@gmail.com>
2026-08-31 10:02:53 +0530

    Time: 63 ms (18.47%) | Memory: 19.1 MB (88.26%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.c
+++ 217-contains-duplicate/contains-duplicate.c
@@ -1,17 +1,13 @@
-#include <stdbool.h>
-#include <stdlib.h>
-
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
 
 bool containsDuplicate(int* nums, int numsSize) {
-    
-    // Sort the array
+
     qsort(nums, numsSize, sizeof(int), compare);
 
-    // Check adjacent elements
     for (int i = 1; i < numsSize; i++) {
+
         if (nums[i] == nums[i - 1]) {
             return true;
         }
