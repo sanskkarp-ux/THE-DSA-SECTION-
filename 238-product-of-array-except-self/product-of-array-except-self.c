a08a331d261186f781fc50ca7eaaeda9867e4939
Sanxkar <sanskkarp@gmail.com>
2026-09-06 02:27:46 +0530

    Time: 3 ms (27.65%) | Memory: 33 MB (5.99%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int* answer = (int*)malloc(numsSize * sizeof(int));
+    int* leftProduct = (int*)malloc(numsSize * sizeof(int));
+    int* rightProduct = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+  
+    leftProduct[0] = 1;  
+    for (int i = 1; i < numsSize; i++) {
+        leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+    }
+    
+    
+    rightProduct[numsSize - 1] = 1;  
+    for (int i = numsSize - 2; i >= 0; i--) {
+        rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+    }
+    
+    for (int i = 0; i < numsSize; i++) {
+        answer[i] = leftProduct[i] * rightProduct[i];
+    }
+    
+
+    return answer;
+}
